fba3faf89527dd62970538bcd61c41648ef0fa79
fcojorgeaguiar <fcojorgeaguiar@gmail.com>
2025-02-13 11:44:03 -0300

    Criei o exercício 16.

---
+++ Aula014/ambiente.js
@@ -0,0 +1,13 @@
+/*
+var c = 1
+while (c <= 5){
+    console.log(c)
+    c++
+}
+*/
+
+console.log('Vai começar a contar')
+for (var c = 1;c<=5;c++){
+    console.log(c)
+}
+console.log('Fim da contagem')
