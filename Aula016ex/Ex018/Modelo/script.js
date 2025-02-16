47e2204bb7f8fdaca9f0b3841b3177a35c1b3e88
fcojorgeaguiar <fcojorgeaguiar@gmail.com>
2025-02-16 13:05:33 -0300

    Interatividade da aula 16 ex

--- Aula016ex/Ex018/Modelo/script.js
+++ Aula016ex/Ex018/Modelo/script.js
@@ -20,8 +20,44 @@ function inLista(n, l){
 
 function adicionar(){
     if(isNumero(num.value) && !inLista(num.value, valores)){
-       
+       valores.push(Number(num.value))
+       let item = document.createElement('option')
+       if (Number(num.value) < 10){
+         item.text = `Valor 0${num.value} adicionado.`
+       } else {
+         item.text = `Valor ${num.value} adicionado.`
+       }
+       lista.appendChild(item)
+       res.innerHTML = ''
     } else {
         window.alert('Valor inválido ou já encontrado na lista!')
     }
+    num.value = ''
+    num.focus()
+}
+
+function finalizar(){
+     if (valores.length == 0){
+      window.alert('Adicione valores antes de finalizar')
+     } else {
+       let total = valores.length 
+       let maior = valores[0]
+       let menor = valores[0]
+       let soma = 0
+       let media = 0
+       for( let pos in valores){
+           soma += valores[pos]
+         if(valores[pos] > maior){
+            maior = valores[pos]
+           }
+           if (valores[pos] < menor){
+            menor = valores[pos]
+           }
+       }
+      media = soma/total
+      res.innerHTML = ''
+      res.innerHTML += `<p>Ao todo temos ${total} números cadastrados.</p><p> São eles: ${valores}.`
+      res.innerHTML += `<p>O maior valor informado é ${maior}.</p><p>O menor valor informado é ${menor}.<p>Somando todos os valores, temos ${soma}.<p>A média dos valores digitados é ${media}.</p>.`
+
+     }
 }
